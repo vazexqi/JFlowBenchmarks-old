1c401dbdb7572ced367ebc79b1541d5b15402fba
Kevin Chen <kevinwchen26@gmail.com>
2012-07-25 15:10:43 -0500

    Removed changes from original benchmark

--- jbzip2/src/edu/illinois/jflow/benchmark/Benchmark.java
+++ jbzip2/src/edu/illinois/jflow/benchmark/Benchmark.java
@@ -6,41 +6,40 @@ import java.io.*;
 
 public class Benchmark {
 
-    // TODO: Do repeated measurements
-    // TODO: Create Google Caliper measurement version
+	// TODO: Do repeated measurements
+	// TODO: Create Google Caliper measurement version
 
-    public static void main(String[] args) throws Exception {
-        for (int i = 0; i < 2; i++) {
-            final File inputFile = new File("inputs/media.dat");
-            final File outputFile = new File("media.compressed.bz2");
+	public static void main(String[] args) throws Exception {
+		final File dir= mkdir("dir");
+		final File inputFile= new File("inputs/media.dat");
+		final File outputFile= new File("media.compressed.bz2");
 
-            InputStream fileInputStream = new BufferedInputStream(new FileInputStream(inputFile));
-            BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(new FileOutputStream(outputFile));
-            BZip2OutputStream compressorStream = new BZip2OutputStream(bufferedOutputStream);
+		InputStream fileInputStream= new BufferedInputStream(new FileInputStream(inputFile));
+		BufferedOutputStream bufferedOutputStream= new BufferedOutputStream(new FileOutputStream(outputFile));
+		BZip2OutputStream compressorStream= new BZip2OutputStream(bufferedOutputStream);
 
-            // Compression - need to use buffered streams or the results will be too I/O intensive
-            long startCompressed = System.currentTimeMillis();
+		// Compression - need to use buffered streams or the results will be too I/O intensive
+		long startCompressed= System.currentTimeMillis();
 
-            byte[] buffer = new byte[5242880];
-            int bytesRead;
-            while ((bytesRead = fileInputStream.read(buffer)) != -1) {
-                compressorStream.write(buffer, 0, bytesRead,Runtime.getRuntime().availableProcessors());
-            }
-            compressorStream.shutDownExecutor();
-            long stopCompressed = System.currentTimeMillis();
-            System.out.println((stopCompressed - startCompressed) + "ms");
+		byte[] buffer= new byte[5242880];
+		int bytesRead;
+		while ((bytesRead= fileInputStream.read(buffer)) != -1) {
+			compressorStream.write(buffer, 0, bytesRead);
+		}
+        compressorStream.shutDownExecutor();
+        long stopCompressed= System.currentTimeMillis();
+        System.out.println((stopCompressed - startCompressed) + "ms");
 
-            fileInputStream.close();
-            compressorStream.close();
-        }
+		fileInputStream.close();
+		compressorStream.close();
 
 
-    }
+	}
 
-    static File mkdir(String name) throws IOException {
-        File f = File.createTempFile(name, "");
-        f.delete();
-        f.mkdir();
-        return f;
-    }
+	static File mkdir(String name) throws IOException {
+		File f= File.createTempFile(name, "");
+		f.delete();
+		f.mkdir();
+		return f;
+	}
 }
