fe2c593aaed01ba7aaaed97624a56025c0cda35e
Kevin Chen <kevinwchen26@gmail.com>
2012-08-06 11:11:49 -0500

    Added JLinkGrammar

---
+++ JLinkGrammar/src/net/sf/jlinkgrammar/Parse.java
@@ -0,0 +1,34 @@
+package net.sf.jlinkgrammar;
+
+import java.io.IOException;
+
+/*
+ * parser.java
+ *
+ * Created on December 1, 2006, 10:00 AM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+/**
+ * @author johnryan
+ */
+public class Parse {
+
+	/**
+	 * Creates a new instance of parser
+	 */
+	public Parse() {
+	}
+
+	/**
+	 * @param args
+	 *            the command line arguments
+	 */
+
+	public static void main(String[] args) throws IOException {
+		Parser.doIt(args);
+	}
+
+}
